985f219d996ccc86a5cd7f1dd1215fd6bd738753
bpotmalnik <bpotmalnik@users.noreply.github.com>
2024-02-24 10:56:02 +0000

    Fix styling

--- src/ReqresConnector.php
+++ src/ReqresConnector.php
@@ -24,15 +24,11 @@ class ReqresConnector extends Connector implements HasPagination
 
     public function paginate(Request $request): PagedPaginator
     {
-        return new class(
-            connector: $this,
-            request: $request
-        ) extends PagedPaginator
+        return new class(connector: $this, request: $request) extends PagedPaginator
         {
             protected function isLastPage(
                 Response $response
-            ): bool
-            {
+            ): bool {
                 return $response->json('page')
                     === $response->json('total_pages');
             }
@@ -40,8 +36,7 @@ class ReqresConnector extends Connector implements HasPagination
             protected function getPageItems(
                 Response $response,
                 Request $request
-            ): array
-            {
+            ): array {
                 return $response->json('data');
             }
         };
