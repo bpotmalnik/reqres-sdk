a7f40d1bfe8024b940318430030728b0664a8555
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2024-02-24 12:24:25 +0000

    feat: add tests with fixtures

---
+++ tests/Unit/Users/GetUserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Bpotmalnik\ReqresSdk\ReqresConnector;
+use Bpotmalnik\ReqresSdk\Requests\Users\GetUser;
+use Saloon\Http\Faking\MockClient;
+use Saloon\Http\Faking\MockResponse;
+
+it('can get user', function () {
+    $mockClient = new MockClient([
+        GetUser::class => MockResponse::fixture('user'),
+    ]);
+
+    $user = (new ReqresConnector)
+        ->withMockClient($mockClient)
+        ->users()
+        ->get('1');
+
+    expect($user->dto())
+        ->toHaveProperties([
+            'id' => 1,
+            'email' => "george.bluth@reqres.in",
+            'first_name' => 'George',
+            'last_name' => 'Bluth',
+            'avatar' => 'https://reqres.in/img/faces/1-image.jpg',
+        ]);
+});
---
+++ tests/Unit/Users/GetUsersTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Bpotmalnik\ReqresSdk\ReqresConnector;
+use Bpotmalnik\ReqresSdk\Requests\Users\GetUsers;
+use Saloon\Http\Faking\MockClient;
+use Saloon\Http\Faking\MockResponse;
+
+it('can get users', function () {
+    $mockClient = new MockClient([
+        GetUsers::class => MockResponse::fixture('users'),
+    ]);
+
+    $users = (new ReqresConnector)
+        ->withMockClient($mockClient)
+        ->users()
+        ->all();
+
+    $users->setPerPageLimit(1);
+    $users->setMaxPages(1);
+
+    expect($users->collect()->first())
+        ->toHaveProperties([
+            'id' => 1,
+            'email' => "george.bluth@reqres.in",
+            'first_name' => 'George',
+            'last_name' => 'Bluth',
+            'avatar' => 'https://reqres.in/img/faces/1-image.jpg',
+        ]);
+});
