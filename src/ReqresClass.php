fd88eeb889435379927a1df1e3a48af27403df35
Bartosz Potmalnik <20015942+bpotmalnik@users.noreply.github.com>
2024-02-24 10:55:37 +0000

    fix: rename reqres class to reqres connector

--- src/ReqresClass.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Bpotmalnik\Reqres;
-
-class ReqresClass
-{
-}
---
+++ src/ReqresConnector.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Bpotmalnik\ReqresSdk;
+
+use Saloon\Http\Connector;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+use Saloon\PaginationPlugin\Contracts\HasPagination;
+use Saloon\PaginationPlugin\PagedPaginator;
+
+class ReqresConnector extends Connector implements HasPagination
+{
+    public function resolveBaseUrl(): string
+    {
+        return 'https://reqres.in/api/';
+    }
+
+    public function defaultHeaders(): array
+    {
+        return [
+            'Content-Type' => 'application/json',
+        ];
+    }
+
+    public function paginate(Request $request): PagedPaginator
+    {
+        return new class(
+            connector: $this,
+            request: $request
+        ) extends PagedPaginator
+        {
+            protected function isLastPage(
+                Response $response
+            ): bool
+            {
+                return $response->json('page')
+                    === $response->json('total_pages');
+            }
+
+            protected function getPageItems(
+                Response $response,
+                Request $request
+            ): array
+            {
+                return $response->json('data');
+            }
+        };
+    }
+}
